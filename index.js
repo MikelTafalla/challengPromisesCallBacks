03700266cccd259f733261b8a8ebeace302919df
Mikel <mikel362d@gmail.com>
2022-12-22 07:48:39 -0800

    adds solution 2

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ if (!userInput) {
 
 const studentDistracted = userInput !== 'coding';
 // Fix bug in code below
-const practiceCoding = () => 
-    new Promise((resolve, reject) => { //BUG 2 Missing 'new' to define Promise and missing 'reject' argument
-        if (studentDistracted) {
-            reject (new Error('Coding stopped - Student is distracted'));
-        }
-        resolve ('We are coding!'); //BUG 3 change return for resolve
-    })
+// const practiceCoding = () => 
+//     new Promise((resolve, reject) => { //BUG 2 Missing 'new' to define Promise and missing 'reject' argument
+//         if (studentDistracted) {
+//             reject (new Error('Coding stopped - Student is distracted'));
+//         }
+//         resolve ('We are coding!'); //BUG 3 change return for resolve
+//     })
 
 
-practiceCoding() //BUG 1 Missing parenthesis so we know it is a function
+// practiceCoding() //BUG 1 Missing parenthesis so we know it is a function
+//     .then((result) => console.log(result))
+//     .catch((err) => console.error('Promise rejected:', 'Error: ' + err));
+
+//Chris' Solution
+const practiceCoding = new Promise((resolve, reject) => {
+    if (!studentDistracted) {
+        resolve("We are coding in promises!")
+    } else {
+        reject("Coding stopped - Student is distracted")
+    }
+})
+practiceCoding
     .then((result) => console.log(result))
     .catch((err) => console.error('Promise rejected:', 'Error: ' + err));
